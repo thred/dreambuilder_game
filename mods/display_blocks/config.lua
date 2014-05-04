537a8fa9ef87da591dc8bdf002bf7eb268c5d7a5
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-05-04 07:43:06 -0400

    disable display_blocks mod's "mese giver" and extraneous uranium objects

--- mods/display_blocks/config.lua
+++ mods/display_blocks/config.lua
@@ -1,8 +1,8 @@
 -- Disable to make the uranium only be "technic"uranium". Enable to add "display_blocks:uranium"
-enable_display_uranium = true
+enable_display_uranium = false
 --Enable to make "technic:uranium" spawning like "display_blocks:uranium".
 technic_uranium_new_ore_gen = true
 --Enable to add a recipe that uses "technic:uranium"
 enable_technic_recipe = true
 --Enable a way to build the mese giver ingame
-enable_mese_giver_recipe = true
+enable_mese_giver_recipe = false
