504d7bbd72598a0539ef476b932ada563e3f473a
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-06-28 22:58:02 -0400

    update streets mod

--- mods/streets/streetshotfix/init.lua
+++ mods/streets/streetshotfix/init.lua
@@ -3,3 +3,7 @@
 ]]
 -- Hotfix #1; April 14, 2014; https://github.com/webdesigner97/streets/issues/11
 minetest.register_alias("stairs:stair_asphalt","streets:stair_asphalt")
+
+-- Hotfix #2; June 27, 2014; https://github.com/webdesigner97/streets/issues/25
+minetest.register_alias("streets:asphalt_stair","streets:stair_asphalt")
+minetest.register_alias("streets:asphalt_slab","streets:slab_asphalt")
