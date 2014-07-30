df982bb9ab44a75c864807ed8c4ee70645900007
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-07-30 14:32:34 -0400

    update plantlife and waving plants

--- mods/waving_plants/init.lua
+++ mods/waving_plants/init.lua
@@ -6,78 +6,42 @@
 -- default stuff
 
 for i = 1, 5 do 
-	local new_grass = plantslib:clone_node("default:grass_"..i)
-		new_grass.waving = 1
-	minetest.register_node(":default:grass_"..i, new_grass)
+	minetest.override_item("default:grass_"..i, { waving = 1 })
 end
 
-local new_grass = plantslib:clone_node("default:junglegrass")
-	new_grass.waving = 1
-minetest.register_node(":default:junglegrass", new_grass)
+minetest.override_item("default:junglegrass", { waving = 1 })
 
 -- farming, farming_plus 
 
 for i = 1, 8 do
-	local new_wheat = plantslib:clone_node("farming:wheat_"..i)
-		new_wheat.waving = 1
-	minetest.register_node(":farming:wheat_"..i, new_wheat)
-
-	local new_cotton = plantslib:clone_node("farming:cotton_"..i)
-		new_cotton.waving = 1
-	minetest.register_node(":farming:cotton_"..i, new_cotton)
+	minetest.override_item("farming:wheat_"..i, { waving = 1 })
+	minetest.override_item("farming:cotton_"..i, { waving = 1 })
 end
 
-local new_weed = plantslib:clone_node("farming:weed")
-	new_weed.waving = 1
-minetest.register_node(":farming:weed", new_weed)
+minetest.override_item("farming:weed", { waving = 1 })
 
 -- Undergrowth modpack
 
-local new_youngtree = plantslib:clone_node("youngtrees:youngtree_top")
-	new_youngtree.waving = 1
-minetest.register_node(":youngtrees:youngtree_top", new_youngtree)
+minetest.override_item("youngtrees:youngtree_top", { waving = 1 })
 
 -- Ferns mod
 
 for i = 1, 3 do
-	local new_fern = plantslib:clone_node("ferns:fern_0"..i)
-		new_fern.waving = 1
-	minetest.register_node(":ferns:fern_0"..i, new_fern)
+	minetest.override_item("ferns:fern_0"..i, { waving = 1 })
 end
 
-local new_fern = plantslib:clone_node("ferns:tree_fern_leaves")
-	new_fern.waving = 1
-minetest.register_node(":ferns:tree_fern_leaves", new_fern)
+minetest.override_item("ferns:tree_fern_leaves", { waving = 1 })
 
 -- Dryplants mod
 
-local new_reed = plantslib:clone_node("dryplants:reedmace_height_2")
-	new_reed.waving = 1
-minetest.register_node(":dryplants:reedmace_height_2", new_reed)
-
-local new_reed = plantslib:clone_node("dryplants:reedmace_height_3")
-	new_reed.waving = 1
-minetest.register_node(":dryplants:reedmace_height_3", new_reed)
-
-local new_reed = plantslib:clone_node("dryplants:reedmace_height_3_spikes")
-	new_reed.waving = 1
-minetest.register_node(":dryplants:reedmace_height_3_spikes", new_reed)
-
-local new_juncus = plantslib:clone_node("dryplants:juncus")
-	new_juncus.waving = 1
-minetest.register_node(":dryplants:juncus", new_juncus)
-
-local new_juncus = plantslib:clone_node("dryplants:juncus_02")
-	new_juncus.waving = 1
-minetest.register_node(":dryplants:juncus_02", new_juncus)
+minetest.override_item("dryplants:reedmace_height_2", { waving = 1 })
+minetest.override_item("dryplants:reedmace_height_3", { waving = 1 })
+minetest.override_item("dryplants:reedmace_height_3_spikes", { waving = 1 })
+minetest.override_item("dryplants:juncus", { waving = 1 })
+minetest.override_item("dryplants:juncus_02", { waving = 1 })
 
 -- Farming plus
 
-local new_leaves = plantslib:clone_node("farming_plus:banana_leaves")
-	new_leaves.waving = 1
-minetest.register_node(":farming_plus:banana_leaves", new_leaves)
-
-local new_leaves = plantslib:clone_node("farming_plus:cocoa_leaves")
-	new_leaves.waving = 1
-minetest.register_node(":farming_plus:cocoa_leaves", new_leaves)
+minetest.override_item("farming_plus:banana_leaves", { waving = 1 })
+minetest.override_item("farming_plus:cocoa_leaves", { waving = 1 })
 
