41da638ba409879ad87876432317bca568cfe571
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2015-08-25 19:14:34 -0400

    disable default lavacooling code reset interval + chance to 5, 15.

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -89,22 +89,16 @@ end
 --
 
 default.cool_lava_source = function(pos)
-	minetest.set_node(pos, {name = "default:obsidian"})
-	minetest.sound_play("default_cool_lava",
-		{pos = pos, max_hear_distance = 16, gain = 0.25})
 end
 
 default.cool_lava_flowing = function(pos)
-	minetest.set_node(pos, {name = "default:stone"})
-	minetest.sound_play("default_cool_lava",
-		{pos = pos, max_hear_distance = 16, gain = 0.25})
 end
 
 minetest.register_abm({
 	nodenames = {"default:lava_flowing"},
 	neighbors = {"group:water"},
-	interval = 1,
-	chance = 2,
+	interval = 5,
+	chance = 15,
 	action = function(...)
 		default.cool_lava_flowing(...)
 	end,
@@ -113,8 +107,8 @@ minetest.register_abm({
 minetest.register_abm({
 	nodenames = {"default:lava_source"},
 	neighbors = {"group:water"},
-	interval = 1,
-	chance = 2,
+	interval = 5,
+	chance = 15,
 	action = function(...)
 		default.cool_lava_source(...)
 	end,
